9912a660cd7515bb5c7a89e6c4f09d3f936aa569
KohamaHayato <007.hamako@gmail.com>
2020-07-15 21:30:32 +0900

    Clinic

---
+++ Clinic.py
@@ -0,0 +1,2 @@
+class Clinic:
+    def __init__(self):
---
+++ Patient.py
@@ -0,0 +1,7 @@
+from Human import *
+
+class Patient:
+    def __init__(self,name,sysmptom,patient_id):
+        super.__init__(name)
+        self.sysmpton = sysmptom
+        self.patient_id = patient_id
